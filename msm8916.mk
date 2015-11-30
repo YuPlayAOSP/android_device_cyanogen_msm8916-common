6abddd78ca6ee1f1016fab6417b3c0cd6cf6ca02
neobuddy89 <neobuddy89@gmail.com>
2015-11-30 08:06:43 -0800

    msm8916-common: Compile FM Radio for Marshmallow
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -95,16 +95,14 @@ PRODUCT_PACKAGES += \
     e2fsck \
     make_ext4fs
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
-
 # IO Scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=row
+
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    libfmjni
 
 # IRSC
 PRODUCT_COPY_FILES += \
