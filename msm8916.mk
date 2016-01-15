bae53fc018097ee81e73aa25e38391df0a5b30b7
Ethan Chen <intervigil@gmail.com>
2016-01-14 16:21:45 -0800

    msm8916: Remove unused items
    * These packages and binaries don't exist anymore
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -72,13 +72,6 @@ PRODUCT_PROPERTY_OVERRIDES +=
     persist.cne.feature=4
 endif
 
-# CRDA
-PRODUCT_PACKAGES += \
-    crda \
-    linville.key.pub.pem \
-    regdbdump \
-    regulatory.bin
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8916 \
@@ -86,11 +79,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8916 \
     libtinyxml \
     memtrack.msm8916
-
-# Filesystem
-PRODUCT_PACKAGES += \
-    e2fsck \
-    make_ext4fs
 
 # FM
 PRODUCT_PACKAGES += \
@@ -138,9 +126,7 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.crda.sh \
-    init.qcom.bt.sh \
-    init.qcom.coex.sh
+    init.qcom.bt.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.rc \
@@ -160,10 +146,6 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     libxml2
-
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
 
 # Wifi
 PRODUCT_PACKAGES += \
